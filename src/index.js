0ca8f84546f7c8e940990af1adbea2281df52a02
Denis <denis.rovdo@yandex.com>
2022-08-03 17:56:09 +0200

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,55 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+    let splitArrays = expr.match(/.{1,10}/g);
+    let removedStars = [];
+    
+    
+    for(let i = 0; i < splitArrays.length; i++ ){
+        removedStars.push((splitArrays[i] === '**********') ? splitArrays[i].replace('**********', ' ') : splitArrays[i].match(/.{1,2}/g));
+    } 
+
+
+    let result = [];
+
+    for ( let value of removedStars){
+
+        let d = [];
+        result.push(d);
+
+        for ( let a of value){
+         
+            if (a === '00'){
+
+                d.push(a.replace('00', ''));
+        
+            }if (a === '10'){
+            
+                d.push(a.replace('10', '.'));
+        
+            }if (a === '11'){
+        
+                d.push(a.replace('11', '-'));
+        
+            }
+        }
+    } 
+
+    let resultArr = result.map(internalArray => internalArray.join(''));
+
+    let resultRow = '';
+    for( key of resultArr){
+       if(key.length == 0 ){
+        resultRow += ' ';
+       }else{
+        resultRow += MORSE_TABLE[key];
+       }
+    }
+
+  return   resultRow;
+
 }
 
 module.exports = {
